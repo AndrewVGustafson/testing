5627f0ffbd22b5813f34b8a56109d9341d0c1c22
Andrew G <f4andrew7@gmail.com>
2023-11-25 06:57:53 -0600

    adjust matrix rows to fit all n

--- matrices.py
+++ matrices.py
@@ -3,17 +3,20 @@ import numpy as np
 def grid(n):
     if float(n**0.5).is_integer():
         length = int(n**0.5)
+        array = np.zeros((length, length), dtype=np.int8)
+
     else:
         length = int(np.floor(n**0.5))
+        array = np.zeros((length+1, length), dtype=np.int8)
 
-    array = np.random.randint(10, size=(length, length))
-    print(n-length)
-        # for _ in range(n-(n**0.5)):
-        #     pass
+
+
+    # for row in range(n-length**2):
+    #     array.
     print(array)
 
 def main():
-    grid(5)
+    grid(8)
 
 
 if __name__ == "__main__":

57d1d5653b33cd70b1ddbcafc5e27e79059d0304
Andrew G <f4andrew7@gmail.com>
2024-10-22 20:25:47 -0500

    added color to terminal

--- wordle/main.py
+++ wordle/main.py
@@ -1,19 +1,25 @@
 from selenium import webdriver 
 from selenium.webdriver.chrome.options import Options
-from auto_solver import AutoSolver
 
+from solvers.auto_solver import AutoSolver
+from solvers.manual_solver import ManualSolver
 
 if __name__ == "__main__":
-    # STARTING_WORDS = ["cones", "trial"]
-    STARTING_WORDS = ["slant"]
-    BETTER_GUESS_CUTOFF = 3
-    GUESSING_INTERVAL = 1.45
-    options = Options()
-    options.add_argument('--log-level=3')
-    options.enable_bidi = True
+    solver = ManualSolver()
+    solver.run_solver()
 
-    driver = webdriver.Chrome(options=options)
-    URL = 'https://speedle.rahuljk.com/'
+
+# if __name__ == "__main__":
+#     # STARTING_WORDS = ["cones", "trial"]
+#     STARTING_WORDS = ["slant"]
+#     BETTER_GUESS_CUTOFF = 3
+#     GUESSING_INTERVAL = 1.45
+#     options = Options()
+#     options.add_argument('--log-level=3')
+#     options.enable_bidi = True
+
+#     driver = webdriver.Chrome(options=options)
+#     URL = 'https://speedle.rahuljk.com/'
     
-    auto_solver = AutoSolver(driver, URL)
-    auto_solver.solve(starting_words=STARTING_WORDS, replay_forever=True)
+#     auto_solver = AutoSolver(driver, URL)
+#     auto_solver.solve(starting_words=STARTING_WORDS, replay_forever=True)
